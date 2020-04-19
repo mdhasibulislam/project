be233ba8984c6c87b68d62af13ad9a2a39cec369
mdhasibulislam <hhasibulislam503@gmail.com>
2020-04-19 20:43:53 +0600

    new project

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function(){
+    
+    
+       // Show or hide the sticky footer button
+    $(window).on('scroll', function(event) {
+        if($(this).scrollTop() > 600){
+            $('.back-to-top').fadeIn(200)
+        } else{
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+    
+    
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+    
+    
+    //===== Counter Up
+    
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+    
+    
+
+    
+});
